d2c103f0b25d00639f83e6dd70db806d79264788
ScorTnT <sangu1115@naver.com>
2025-08-10 17:11:47 +0000

    [ai] 30 features of wav data to training

--- code/main/train30.py
+++ code/main/train30.py
@@ -9,18 +9,20 @@ data4train = np.loadtxt(data4train_path, delimiter=',', dtype=np.float32)
 data4test = np.loadtxt(data4test_path, delimiter=',', dtype=np.float32)
 
 train_images = data4train[:,0:-1]
-train_labels = data4train[:,[-1]]
+train_labels = data4train[:,-1]
 test_images = data4test[:,0:-1]
-test_labels = data4test[:,[-1]]
+test_labels = data4test[:,-1]
 
 network = models.Sequential()
 network.add(layers.Dense(128, activation='relu', input_shape=(30,)))
-network.add(layers.Dropout(0.4))
+network.add(layers.Dropout(0.2))
 network.add(layers.Dense(64, activation='relu'))
-network.add(layers.Dropout(0.5))
-network.add(layers.Dense(16, activation='relu'))
-network.add(layers.Dense(2, activation='sigmoid'))
-network.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+network.add(layers.Dropout(0.3))
+network.add(layers.Dense(32, activation='relu'))
+network.add(layers.Dense(4, activation='relu'))
+network.add(layers.Dense(2, activation='softmax'))
+network.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# network.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
